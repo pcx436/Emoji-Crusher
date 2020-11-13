4c8ff67e034eb817a79573062cb467d532fe1e67
Vince <vincentliu362@gmail.com>
2020-11-12 19:33:23 -0700

    init database function

--- src/com/emojiCrusher/Model.java
+++ src/com/emojiCrusher/Model.java
@@ -23,4 +23,33 @@ public class Model {
         createDB();
     }
 
+    private void createDB() {
+        try {
+            Statement connection1 = null;
+            Statement connection2 = null;
+            database = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened Database successfully");
+
+            connection1 = database.createStatement();
+            String command = "CREATE TABLE `scoreboard` (" +
+                    "    `id`    INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "    `name`    TEXT NOT NULL," +
+                    "    `score`    INTEGER NOT NULL" +
+                    ");";
+            connection1.executeUpdate(command);
+
+            connection2 = database.createStatement();
+            command = "CREATE TABLE `EmojiPool` (\n" +
+                    "    `id`    INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "    `path`    TEXT NOT NULL UNIQUE\n" +
+                    ");";
+            connection2.executeUpdate(command);
+
+            System.out.println("Created both tables.");
+
+        } catch (Exception e) {
+            System.out.println("ERROR: Database Connection" + e.getMessage());
+        }
+    }
+
 }
