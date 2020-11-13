6362a4225e1397544c2da4dc6c68ddaebf8b81d1
Vince <vincentliu362@gmail.com>
2020-11-12 19:32:35 -0700

    Fill in Model constructors

--- src/com/emojiCrusher/Model.java
+++ src/com/emojiCrusher/Model.java
@@ -1,2 +1,26 @@
-package com.emojiCrusher;public class Model {
+package com.emojiCrusher;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
+public class Model {
+    public List<List<String>> getScoreTable() {
+        return scoreTable;
+    }
+
+    private int maxEmojis;
+    private int maxScoreDisplay;
+    private ImageIcon[] emojis;
+    private Connection database;
+    private List<List<String>> scoreTable;
+
+    public Model(int maxEmojis, int maxScores) {
+        this.maxEmojis = maxEmojis;
+        this.maxScoreDisplay = maxScores;
+        scoreTable = new ArrayList();
+        createDB();
+    }
+
 }
