cb4ac404a8480006e692dad39ae4acdbc65d7751
Jacob Malcy <pcx436@yahoo.com>
2020-11-13 18:04:40 -0700

    Add scoreboard to controller

--- src/com/emojiCrusher/Controller.java
+++ src/com/emojiCrusher/Controller.java
@@ -3,23 +3,40 @@ package com.emojiCrusher;
 public class Controller {
     private final EmojiSelect emojiSelect;
     private final GameInterface gameInterface;
+    private final ScoreBoard scoreBoard;
     private final MainMenu mainMenu;
 
     public Controller() {
         emojiSelect = new EmojiSelect();
         gameInterface = new GameInterface();
+        scoreBoard = new ScoreBoard();
         mainMenu = new MainMenu();
 
+        quitBehavior();
+
+        initMain();
+
+        mainMenu.getFrame().setVisible(true);
+    }
+
+    private void quitBehavior() {
         emojiSelect.getQuitButton().addActionListener(actionEvent -> {
             emojiSelect.getFrame().setVisible(false);
             mainMenu.getFrame().setVisible(true);
         });
 
-        gameInterface.getQuitGame().addActionListener(actionEvent -> {
+        gameInterface.getQuitButton().addActionListener(actionEvent -> {
             gameInterface.getFrame().setVisible(false);
             mainMenu.getFrame().setVisible(true);
         });
 
+        scoreBoard.getQuitButton().addActionListener(actionEvent -> {
+            scoreBoard.getFrame().setVisible(false);
+            mainMenu.getFrame().setVisible(true);
+        });
+    }
+
+    private void initMain() {
         mainMenu.getEmojiPickerButton().addActionListener(actionEvent -> {
             emojiSelect.getFrame().setVisible(true);
             mainMenu.getFrame().setVisible(false);
@@ -28,9 +45,11 @@ public class Controller {
             gameInterface.getFrame().setVisible(true);
             mainMenu.getFrame().setVisible(false);
         });
-        mainMenu.getExitButton().addActionListener(actionEvent -> System.exit(0));
-
-        mainMenu.getFrame().setVisible(true);
+        mainMenu.getScoreboardButton().addActionListener(actionEvent -> {
+            scoreBoard.getFrame().setVisible(true);
+            mainMenu.getFrame().setVisible(false);
+        });
+        mainMenu.getQuitButton().addActionListener(actionEvent -> System.exit(0));
     }
 
 }
