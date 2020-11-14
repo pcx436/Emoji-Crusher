3ffaca1f9094df2f5035b8c6e2b4bf2972d1fe31
Vince <vincentliu362@gmail.com>
2020-11-13 19:29:43 -0700

    able to modify the Scoretable

--- src/com/emojiCrusher/Controller.java
+++ src/com/emojiCrusher/Controller.java
@@ -1,16 +1,21 @@
 package com.emojiCrusher;
 
+import javax.swing.*;
+import java.util.List;
+
 public class Controller {
     private final EmojiSelect emojiSelect;
     private final GameInterface gameInterface;
     private final ScoreBoard scoreBoard;
     private final MainMenu mainMenu;
+    private Model model;
 
     public Controller() {
         emojiSelect = new EmojiSelect();
         gameInterface = new GameInterface();
         scoreBoard = new ScoreBoard();
         mainMenu = new MainMenu();
+        model = new Model(5, 10);
 
         quitBehavior();
 
@@ -48,6 +53,14 @@ public class Controller {
         mainMenu.getScoreboardButton().addActionListener(actionEvent -> {
             scoreBoard.getFrame().setVisible(true);
             mainMenu.getFrame().setVisible(false);
+            List<List<String>> res = model.getScoreTable();
+            String [][]table = new String[res.size()][2];
+            for(int i = 0; i < res.size(); i++){
+                table[i][0] = res.get(i).get(0);
+                table[i][1] = res.get(i).get(1);
+            }
+            String column[] = {"Name", "Score"};
+            scoreBoard.setScoreTable(new JTable(table, column));
         });
         mainMenu.getQuitButton().addActionListener(actionEvent -> System.exit(0));
     }
